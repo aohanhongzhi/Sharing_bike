18241fb304b421366735a38abae350628765b7c7
xiaomaomiao <1051346747@qq.com>
2017-11-27 10:43:31 +0800

    远程同步

--- src/main/java/cn/sharing_bike/controller/PageJumpController.java
+++ src/main/java/cn/sharing_bike/controller/PageJumpController.java
@@ -1,22 +1,15 @@
 package cn.sharing_bike.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import cn.sharing_bike.service.HelloService;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+/**
+ * 页面跳转控制器
+ * @author JustCute
+ *
+ */
 @Controller
-public class HelloController {
-	@Autowired
-	private HelloService helloService;
-
-	@RequestMapping("/index")
-	public String hello() throws Exception{
-		System.out.println(helloService.queryHello().getHello());
-		return "forward:/index.jsp";
-	}
+public class PageJumpController {
 
 	@RequestMapping(value = "/show_map",method = RequestMethod.GET)
 	public String show_map() throws Exception{
